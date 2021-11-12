4bc37a3a716bd90a1eb48db54121206abd7bff6f
Tanerbyk <tanerbuyuk06@gmail.com>
2021-11-12 03:15:46 +0300

    Revert "Revert ".""
    This reverts commit d459f37525427d083679e0465911f0dc7e1e7483.

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer w)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult result = wv.Validate(w);
+            if (result.IsValid)
+            {
+                w.WriterStatus = true;
+                w.WriterAbout = "Deneme";
+                wm.WriterAdd(w);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+    }
+}
